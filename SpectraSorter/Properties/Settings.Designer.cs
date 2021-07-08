693c0bad14277de626d50f126cabf13e2560fbba
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2021-07-08 18:05:06 +0200

    Change default acquisition parameters.

--- SpectraSorter/Properties/Settings.Designer.cs
+++ SpectraSorter/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace spectra.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -37,7 +37,7 @@ namespace spectra.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("10")]
+        [global::System.Configuration.DefaultSettingValueAttribute("250")]
         public uint IntegrationTime {
             get {
                 return ((uint)(this["IntegrationTime"]));
@@ -49,7 +49,7 @@ namespace spectra.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("50000")]
+        [global::System.Configuration.DefaultSettingValueAttribute("15")]
         public uint BackToBackPerTrigger {
             get {
                 return ((uint)(this["BackToBackPerTrigger"]));
@@ -61,7 +61,7 @@ namespace spectra.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("15")]
         public uint NumSpectraPerRequest {
             get {
                 return ((uint)(this["NumSpectraPerRequest"]));
